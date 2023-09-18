15add4f65593c798165211560338918243d0dd3f
Alex793x <alex793x@stud.kea.dk>
2023-09-18 02:01:07 +0200

    added links when changing input field!

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -4,8 +4,13 @@ export const setupDropDown = () => {
     const selectElement = document.getElementById("ddKommuner");
     const buttonElement = document.getElementById("pbFetchKommuner");
 
-    buttonElement.addEventListener("click",
-        async() => await fetchKommune(urlKommune, selectElement));
+    buttonElement.addEventListener("click", async () => {
+        await fetchKommune(urlKommune, selectElement);
+    });
+
+    selectElement.addEventListener("change", () => {
+        addKommuneHref(selectElement);
+    });
 }
 
 export const fetchKommune = async (url, selectElement) => {
@@ -17,16 +22,29 @@ export const fetchKommune = async (url, selectElement) => {
     await addKommuneNameForSelect(data, selectElement);
 }
 
-
 const addKommuneNameForSelect = (data, selectToFill) => {
     data.forEach((item, index) => {
         if (item.hasOwnProperty("navn")) {
             const option = document.createElement("option");
             option.textContent = item.navn.toString();
             option.value = index;
-            option.href = item.href;
-            console.log(option.href)
+            option.dataset.href = item.href; // Store the href as a data attribute
             selectToFill.appendChild(option);
         }
     });
+}
+
+const addKommuneHref = (selectElement) => {
+    const selectedOption = selectElement.options[selectElement.selectedIndex];
+    const href = selectedOption.dataset.href; // Retrieve href from data attribute
+    if (href) {
+        const anchor = document.createElement("a");
+        anchor.textContent = selectedOption.text.toString();
+        anchor.href = href; // Set the href for the anchor element
+        anchor.target = "_blank"; // Open the link in a new tab
+        const newLine = document.createElement("br");
+        const appElement = document.getElementById("app");
+        appElement.appendChild(newLine);
+        appElement.appendChild(anchor);
+    }
 }
